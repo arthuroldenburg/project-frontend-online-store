e4be3ab0ada573b6d50b2e6e85d355cf6e2fcae0
Sophia <sophiasiqueiram@gmail.com>
2022-11-09 17:07:41 -0300

    Requisito 4 - correção lint

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,34 +1,33 @@
 import React from 'react';
-import { getCategories } from '../services/api'
+import { getCategories } from '../services/api';
 
 class Categories extends React.Component {
   constructor() {
     super();
     this.state = {
       categoriesList: [],
-    }
+    };
   }
 
   async componentDidMount() {
     const categories = await getCategories();
     this.setState({ categoriesList: categories });
-    console.log(categories);
   }
 
   render() {
     const { categoriesList } = this.state;
-    console.log(categoriesList)
     return (
       <div>
         <h1>Categorias:</h1>
         { categoriesList.map(({ name, id }) => (
           <button
-          data-testid="category"
-          key={ id }
+            type="button"
+            data-testid="category"
+            key={ id }
           >
             { name }
           </button>
-        ) ) }
+        ))}
       </div>
     );
   }
