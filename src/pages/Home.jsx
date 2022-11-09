48f57abbc32cde0a9df1b716b408d70a4431f2b1
Sophia <sophiasiqueiram@gmail.com>
2022-11-09 17:03:51 -0300

    Requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Categories from '../components/Categories';
 
 function Home() {
   return (
-    <label htmlFor="campo-pesquisa">
-      <input type="text" />
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
-      <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
-    </label>
+    <div>
+      <label htmlFor="campo-pesquisa">
+        <input type="text" />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
+      </label>
+      <Categories />
+      {/* <p>banana</p> */}
+    </div>
   );
 }
 
